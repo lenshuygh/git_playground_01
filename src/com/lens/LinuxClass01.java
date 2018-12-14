7a2eab7b16d477439637ecc36b34066397ab21ab
lens <lens.huygh@gmail.com>
2018-12-14 14:20:43 +0100

    Linux commit 01 - to be pulled into osX after changes are made to the project on osX

---
+++ src/com/lens/LinuxClass01.java
@@ -0,0 +1,7 @@
+package com.lens;
+
+public class LinuxClass01 {
+    public LinuxClass01(){
+        System.out.println(this + " default constructor");
+    }
+}
--- src/com/lens/TestGit01.java
+++ src/com/lens/TestGit01.java
@@ -3,5 +3,6 @@ package com.lens;
 public class TestGit01 {
     public static void main(String[] args) {
         System.out.println("this is the first class");
+        LinuxClass01 linuxClass01 = new LinuxClass01();
     }
 }
